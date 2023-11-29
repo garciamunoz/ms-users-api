1312754e196cad5a355a12beffef7264166425ab
Luis Garcia <garciala@globalhitss.com>
2023-11-28 20:56:22 -0500

    first commit

--- src/main/java/com/pe/sermaluc/business/UsuarioService.java
+++ src/main/java/com/pe/sermaluc/business/UsuarioService.java
@@ -1,16 +1,14 @@
 package com.pe.sermaluc.business;
 
-<<<<<<< HEAD
+
 import com.pe.sermaluc.bean.User;
-=======
->>>>>>> origin/main
+
 import com.pe.sermaluc.dto.UsuarioDTO;
 
 public interface UsuarioService {
 
-<<<<<<< HEAD
+
     public UsuarioDTO registrarUsuario(String token, User user);
-=======
-    //public UsuarioDTO registrarUsuario(String token, );
->>>>>>> origin/main
+    
+
 }
